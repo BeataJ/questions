e20227d0842efc6b422fd9789001e7d0ba9ac60a
beata <beata@emc2wave.com>
2020-07-29 14:20:38 -0700

    get currentTarget parentElement and toggle

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ btns = document.querySelectorAll('.question-btn');
 
 btns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    console.log(e.currentTarget);
+    const question = e.currentTarget.parentElement.parentElement;
+
+    question.classList.toggle('show-text');
   });
 });
