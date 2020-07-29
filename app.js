ac04ceb4169b01ee57bf176562789f85a9b4086f
beata <beata@emc2wave.com>
2020-07-29 14:12:34 -0700

    selsct all btns add event listener

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 //using selectors inside the element
 // traversing the dom
+
+btns = document.querySelectorAll('.question-btn');
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    console.log(e.currentTarget);
+  });
+});
